84a1952c1c13eff432f77347224db36a2b241292
Brian <nieh_brian@yahoo.com>
2026-02-17 11:51:01 -0800

    Adxl testing data and MPR121 start test

--- Payload_Teensy_sketch.ino
+++ Payload_Teensy_sketch.ino
@@ -88,67 +88,6 @@ void displayDataRate(void)
   Serial.println(" Hz");
 }
 
-void displayDataRate(void)
-{
-  Serial.print("Data Rate:    ");
-
-  switch (imu.getDataRate())
-  {
-  case ADXL343_DATARATE_3200_HZ:
-    Serial.print("3200 ");
-    break;
-  case ADXL343_DATARATE_1600_HZ:
-    Serial.print("1600 ");
-    break;
-  case ADXL343_DATARATE_800_HZ:
-    Serial.print("800 ");
-    break;
-  case ADXL343_DATARATE_400_HZ:
-    Serial.print("400 ");
-    break;
-  case ADXL343_DATARATE_200_HZ:
-    Serial.print("200 ");
-    break;
-  case ADXL343_DATARATE_100_HZ:
-    Serial.print("100 ");
-    break;
-  case ADXL343_DATARATE_50_HZ:
-    Serial.print("50 ");
-    break;
-  case ADXL343_DATARATE_25_HZ:
-    Serial.print("25 ");
-    break;
-  case ADXL343_DATARATE_12_5_HZ:
-    Serial.print("12.5 ");
-    break;
-  case ADXL343_DATARATE_6_25HZ:
-    Serial.print("6.25 ");
-    break;
-  case ADXL343_DATARATE_3_13_HZ:
-    Serial.print("3.13 ");
-    break;
-  case ADXL343_DATARATE_1_56_HZ:
-    Serial.print("1.56 ");
-    break;
-  case ADXL343_DATARATE_0_78_HZ:
-    Serial.print("0.78 ");
-    break;
-  case ADXL343_DATARATE_0_39_HZ:
-    Serial.print("0.39 ");
-    break;
-  case ADXL343_DATARATE_0_20_HZ:
-    Serial.print("0.20 ");
-    break;
-  case ADXL343_DATARATE_0_10_HZ:
-    Serial.print("0.10 ");
-    break;
-  default:
-    Serial.print("???? ");
-    break;
-  }
-  Serial.println(" Hz");
-}
-
 void setup()
 {
   delay(5000);
@@ -170,6 +109,9 @@ void setup()
 
   Wire.begin(); // defaults to SDA=18, SCL=19 on Teensy 4.1
 
+  Serial.println("ADXL375 Accelerometer Test");
+  Serial.println("");
+
   if (!imu.begin())
   { // use the I2C interface over STEMMA QT
     Serial.println("ADXL375 not detected!");
@@ -178,10 +120,33 @@ void setup()
   }
 
   Serial.println("Normal");
+  imu.printSensorDetails();
+
+  displayDataRate();
+  Serial.println("");
+
+  if (!cap.begin())
+  {
+    Serial.println("MPR121 not detected!");
+    while (1)
+      ;
+  }
 }
 
 void loop()
 {
   sensors_event_t event;
   imu.getEvent(&event);
+
+  Serial.print("X: ");
+  Serial.print(event.acceleration.x);
+  Serial.print("  ");
+  Serial.print("Y: ");
+  Serial.print(event.acceleration.y);
+  Serial.print("  ");
+  Serial.print("Z: ");
+  Serial.print(event.acceleration.z);
+  Serial.print("  ");
+  Serial.println("m/s^2 ");
+  delay(500);
 }
