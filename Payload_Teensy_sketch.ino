a8caa5aa8388786734a4be24dc6cb791893cf262
Brian <nieh_brian@yahoo.com>
2026-02-18 15:10:24 -0800

    Testing comment

--- Payload_Teensy_sketch.ino
+++ Payload_Teensy_sketch.ino
@@ -159,15 +159,15 @@ void loop()
   sensors_event_t event;
   // accel.getEvent(&event);
 
-  Serial.print("X: ");
-  Serial.print(event.acceleration.x);
-  Serial.print("  ");
-  Serial.print("Y: ");
-  Serial.print(event.acceleration.y);
-  Serial.print("  ");
-  Serial.print("Z: ");
-  Serial.print(event.acceleration.z);
-  Serial.print("  ");
-  Serial.println("m/s^2 ");
+  // Serial.print("X: ");
+  // Serial.print(event.acceleration.x);
+  // Serial.print("  ");
+  // Serial.print("Y: ");
+  // Serial.print(event.acceleration.y);
+  // Serial.print("  ");
+  // Serial.print("Z: ");
+  // Serial.print(event.acceleration.z);
+  // Serial.print("  ");
+  // Serial.println("m/s^2 ");
   delay(500);
 }
